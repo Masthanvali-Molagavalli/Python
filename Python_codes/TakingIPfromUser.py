46c9d1a269032217797975b4be63282ab9398cbf
masthanvali-molagavalli <masthanvali.molagavalli@gmail.com>
2025-03-20 22:02:12 +0530

    Added arithmetic operation of two input numbers from the user

---
+++ Python_codes/TakingIPfromUser.py
@@ -0,0 +1,20 @@
+print("Welcome to python world");
+x = input("Enter A value:");
+y = input("Enter B value:");
+z = float(x) + float(y);
+z = round(z);
+p = float(x) - float(y);
+p = round(p);
+q = float(x) * float(y);
+q = round(q);
+a = float(x) % float(y);
+a = round(a);
+b = float(x) / float(y);
+b = round(b);
+
+print("The Addition of X + Y is :-",z);
+print("The Subtraction of X - Y is :-",p);
+print("The Multiplication of X * Y is :-",q);
+print("The Modulos of X % Y is :-",a);
+print("The Division of X / Y is :-", b);
+
